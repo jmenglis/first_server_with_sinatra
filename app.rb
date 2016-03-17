73c490c72400f9178ec3713728f55902a19e702a
Josh English <josh@joshenglish.com>
2016-03-17 12:11:29 -0500

    Sinatra added change

--- app.rb
+++ app.rb
@@ -4,13 +4,13 @@ Bundler.require # loads all the gems
 get '/' do
   phamason product = {
     :name => 'New keyboard',
-    :quantity => 17
+    :quantity => 17,
     :description => 'Generic Keyboard',
     :price => '5.99'
   }.to_json # will turn object into string // servers communicate with strings
 end
 
-  not_found do {
+not_found do {
       :message => 'The resource you requested could not be found'
     }.to_json
 end
